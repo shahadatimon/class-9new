9f26b7c2bd29ed484093b30dc6143d30a22d1f55
shahadatimon <shahadutimon2015@gmail.com>
2021-12-14 22:13:41 +0600

    new file

---
+++ app.js
@@ -0,0 +1,203 @@
+
+
+                                /**this is 5 grate ruselt sheet */
+const js = [
+    {
+        name    : `shahadat`,
+        roll    : 01,
+        year    : 15,
+        bangla  : 88,
+        english : 56,
+        math    : 99,
+        scince  : 77,
+    },
+    {
+        name    : `jillur rahman`,
+        roll    : 02,
+        year    : 18,
+        bangla  : 85,
+        english : 58,
+        math    : 79,
+        scince  : 67,
+    },
+    {
+        name    : `redoy khan`,
+        roll    : 03,
+        year    : 20,
+        bangla  : 75,
+        english : 58,
+        math    : 49,
+        scince  : 57,
+    },
+    {
+        name    : `nayim`,
+        roll    : 04,
+        year    : 12,
+        bangla  : 75,
+        english : 58,
+        math    : 49,
+        scince  : 87,
+    },
+    {
+        name    : `rumman`,
+        roll    : 05,
+        year    : 15,
+        bangla  : 95,
+        english : 48,
+        math    : 59,
+        scince  : 87,
+    },
+    {
+        name    : `khadeja`,
+        roll    : 06,
+        year    : 14,
+        bangla  : 85,
+        english : 88,
+        math    : 39,
+        scince  : 87,
+    },
+    {
+        name    : `swopnil`,
+        roll    : 07,
+        year    : 11,
+        bangla  : 45,
+        english : 58,
+        math    : 59,
+        scince  : 37,
+    },
+    {
+        name    : `rakib`,
+        roll    : 08,
+        year    : 13,
+        bangla  : 84,
+        english : 88,
+        math    : 49,
+        scince  : 27,
+    },
+    {
+        name    : `ebrahim`,
+        roll    : 09,
+        year    : 14,
+        bangla  : 85,
+        english : 54,
+        math    : 59,
+        scince  : 87,
+    },
+    {
+        name    : `soniya`,
+        roll    : 10,
+        year    : 11,
+        bangla  : 55,
+        english : 58,
+        math    : 89,
+        scince  : 87,
+    },
+];
+
+let gpa = JSON.stringify(js);
+
+                                      /**this is femily phone number list */
+
+const phone = [
+    {
+        name      : `shahadat`,
+        relation  : `me`,
+        phone     : `01789545544` 
+    },
+    {
+        name      : `rumman`,
+        relation  : `c.bro`,
+        phone     : `01786545564` 
+    },
+    {
+        name      : `rakib`,
+        relation  : `brother`,
+        phone     : `0184515544` 
+    },
+    {
+        name      : `redoy`,
+        relation  : `colic`,
+        phone     : `01952154544` 
+    },
+    {
+        name      : `nayem`,
+        relation  : `colic.sir`,
+        phone     : `01521454544` 
+    },
+    {
+        name      : `hamida`,
+        relation  : `mother`,
+        phone     : `018215654544` 
+    },
+    {
+        name      : `sahin`,
+        relation  : `father`,
+        phone     : `01545654544` 
+    },
+];
+
+let ph = JSON.stringify(phone);
+
+                                     /**developer data hear  */
+ 
+ const devs = [
+     {
+         name   : `shahadat`,
+         year   : 22,
+         skill  : `MERN stack`
+     },
+     {
+         name   : `jillur`,
+         year   : 25,
+         skill  : `WordPress`
+     },
+     {
+         name   : `rumman`,
+         year   : 29,
+         skill  : ` Laravel`
+     },
+     {
+         name   : `rakib`,
+         year   : 20,
+         skill  : `Django`
+     },
+     {
+         name   : `rani`,
+         year   : 20,
+         skill  : `java`
+     },
+     {
+         name   : `emran`,
+         year   : 28,
+         skill  : `GoLan`
+     },
+     {
+         name   : `redoy`,
+         year   : 38,
+         skill  : ` java`
+     },
+     {
+         name   : `sahin`,
+         year   : 26,
+         skill  : `GoLan`
+     },
+     {
+         name   : `ebrahim`,
+         year   : 36,
+         skill  : `Django`
+     },
+     {
+         name   : `reaz`,
+         year   : 25,
+         skill  : `java`
+     },
+ ];  
+ 
+ let dv = JSON.stringify(devs);
+
+ let alldata = [(gpa),(ph),(dv)];
+
+
+ console.log(alldata);
+
+
